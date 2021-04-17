715a429d06f41b59e39239492f88ecb693535b79
Diego Suarez <disofenix117@hotmail.com>
2021-04-17 17:27:30 -0500

    update

---
+++ Assets/Scripts/ObjectInteract.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteract : MonoBehaviour
+{
+    public Inventario inventario;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventario= GameObject.FindGameObjectWithTag("Player").GetComponent<Inventario>();
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Player")
+        {
+            inventario.Cantidad= inventario.Cantidad + 1;
+        Destroy(gameObject);
+        }
+    }
+    
+
+  
+}
