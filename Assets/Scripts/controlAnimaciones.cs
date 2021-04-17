b49d9effe083554fab1dea2ff0f8be016bc895a5
Diego Suarez <disofenix117@hotmail.com>
2021-04-17 18:38:56 -0500

    animaciones movimientos

---
+++ Assets/Scripts/controlAnimaciones.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlAnimaciones : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if( Input.GetAxis("Horizontal")!=0 || Input.GetAxis("Vertical")!=0)
+        {
+            animator.SetBool("caminar", true);
+            
+        }
+        else
+        {
+               animator.SetBool("caminar", false);
+
+        }
+        if(Input.GetKey("e"))
+        {
+            animator.SetTrigger("recoger");
+        }
+        
+        
+
+    }
+
+    public void Empujar(bool act)
+{
+    if(act==true)
+    {
+     animator.SetBool("empujar", true);
+    }
+    else
+    {
+        animator.SetBool("empujar", false);
+    }
+
+}
+
+}
---
+++ Assets/Scripts/pushhingobjects.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pushhingobjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+      Animator animator;
+      controlAnimaciones ca;
+ void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public float pushpower=2.0f;
+
+private void OnControllerColliderHit(ControllerColliderHit hit)
+{
+    Rigidbody body=hit.collider.attachedRigidbody;
+    if(body==null||body.isKinematic)
+
+    {
+            return;
+
+    }
+    if(hit.moveDirection.y<-0.3)
+    {
+        return;
+
+    }
+    else
+    {
+    //ca.Empujar(true);
+        Vector3 pushDir =new Vector3(hit.moveDirection.x,0,hit.moveDirection.z);
+        body.velocity=pushDir*pushpower;
+
+    }
+    
+     //ca.Empujar(false);
+}
+
+
+}
