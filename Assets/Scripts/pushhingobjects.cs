b3d84e05f6432368ca454d00ed9dc1ba3a85314f
Diego Suarez <disofenix117@hotmail.com>
2021-04-22 16:36:46 -0500

    actualizaciones empujar movimientos

--- Assets/Scripts/pushhingobjects.cs
+++ Assets/Scripts/pushhingobjects.cs
@@ -12,31 +12,52 @@ public class pushhingobjects : MonoBehaviour
         animator = GetComponent<Animator>();
     }
     public float pushpower=2.0f;
+    private float targetmass;
+   bool push=false;
 
 private void OnControllerColliderHit(ControllerColliderHit hit)
 {
     Rigidbody body=hit.collider.attachedRigidbody;
     if(body==null||body.isKinematic)
-
     {
+        push=false;
             return;
 
     }
     if(hit.moveDirection.y<-0.3)
     {
+        push=false;
         return;
 
     }
     else
     {
-    //ca.Empujar(true);
+         push=true;
+        targetmass=body.mass;
         Vector3 pushDir =new Vector3(hit.moveDirection.x,0,hit.moveDirection.z);
-        body.velocity=pushDir*pushpower;
+        body.velocity=pushDir*pushpower/targetmass;
+
 
     }
+       
+
     
      //ca.Empujar(false);
 }
+  void Update()
+    {
 
+        if(push==true)
+        {
+            animator.SetBool("empujar", true);
+        }
+        else
+        {
+            animator.SetBool("empujar", false);
+
+        }
+        
+        
+    }
 
 }
